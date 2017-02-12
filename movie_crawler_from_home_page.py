80042e7599122bd0d1ffb85cb6c5c8166d662288
Su Lingfeng <568925701@qq.com>
2017-02-12 23:23:47 +0800

    update project structure, store movie to redis

--- movie_crawler_from_home_page.py
+++ movie_crawler_from_home_page.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import Common.CrawlerToHTML
-import Common.CommonMovieCrawler
-import Common.FileCommon
+import common.crawler_to_html
+import common.common_movie_crawler
+import common.file_common
 import os
 import sys
 
@@ -13,7 +13,7 @@ def compile_url(url):
 
 
 def get_movie_list(url, decode_type='utf-8'):
-    movie_link_list = Common.CrawlerToHTML.get_links_from_html_keyword(url, '/html/gndy/dyzz/2016', decode_type)
+    movie_link_list = common.crawler_to_html.get_links_from_html_keyword(url, '/html/gndy/dyzz/2016', decode_type)
     movie_link_list = list(map(compile_url, movie_link_list))
     movie_link_set = set(movie_link_list)
     final_movie_link_list = list(movie_link_set)
@@ -23,9 +23,9 @@ def get_movie_list(url, decode_type='utf-8'):
 def main():
     my_index_url = 'http://www.dytt8.net/'
     my_real_movie_link_list = get_movie_list(my_index_url, 'gbk')
-    search_movie_download_list = Common.CommonMovieCrawler.get_movie_download_list(my_real_movie_link_list, 'gbk',
-                                                                                   False)
-    Common.FileCommon.write_result_to_txt(search_movie_download_list, os.path.abspath('.') + '/top_movies',
+    search_movie_download_list = common.common_movie_crawler.get_movie_download_list(my_real_movie_link_list, 'gbk',
+                                                                                     False)
+    common.file_common.write_result_to_txt(search_movie_download_list, os.path.abspath('.') + '/top_movies',
                                           'movies2016.txt')
     # tv_url = 'http://www.ygdy8.com/html/tv/oumeitv/20151007/49245.html'
     # tv_list = get_tv_drama_program_list(tv_url, 'gbk')
